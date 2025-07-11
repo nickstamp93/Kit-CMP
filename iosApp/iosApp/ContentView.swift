fe5efee0d81953a29ebcd288548e624d1d815955
nickstamp <nickstamp93@gmail.com>
2025-07-11 16:24:26 +0300

    Adjust iOS project configuration for KitCMP framework

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,6 +1,6 @@
 import UIKit
 import SwiftUI
-import ComposeApp
+import KitCMP
 
 struct ComposeView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
